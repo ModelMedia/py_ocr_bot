d0bdbdf8f27421c25ee856dab2d9685ff9d2fcdb
alexeytas <alexdali@gmail.com>
2020-11-22 22:53:23 +0400

    feat: add module log_srv.py; refactor: module ocr.py

--- ocr.py
+++ ocr.py
@@ -1,11 +1,15 @@
 import os
 import requests
+import log_srv
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+logger = log_srv.get_logger(__name__)
 OCR_API = os.getenv('OCR_API')
+
+url_api = 'https://api.ocr.space/parse/image'
 
 
 def ocr_space_file(filename, overlay=False, language='eng', ocrengine=1):
@@ -30,12 +34,33 @@ def ocr_space_file(filename, overlay=False, language='eng', ocrengine=1):
                'language': language,
                'OCREngine': ocrengine,
                }
-    with open(filename, 'rb') as f:
-        r = requests.post('https://api.ocr.space/parse/image',
-                          files={filename: f},
-                          data=payload,
-                          )
-    return r.content.decode()
+
+    """
+    payload = {'isOverlayRequired': overlay,
+               'language': language,
+               'OCREngine': ocrengine,
+               }
+    files = [
+            ('file',('ru1.png', open('E:/temp/ru1.png', 'rb'),'image/png'))
+            ]
+    headers = {
+              'apikey': OCR_API
+            }
+    """
+    try:
+        with open(filename, 'rb') as f:
+            r = requests.post('https://api.ocr.space/parse/image', files={filename: f}, data=payload,)
+        # r = requests.request("POST", url_api, headers=headers, data=payload, files=files)
+        logger.info('filename: ' + filename)
+        logger.info('response.status_code: ' + str(r.status_code))
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as res_HTTPError:
+        #print('res_HTTPError: ' + str(res_HTTPError))
+        logger.warning('res_HTTPError: ' + str(res_HTTPError))
+    except requests.exceptions.ConnectionError as res_ConnectionError:
+        logger.warning('res_ConnectionError: ' + str(res_ConnectionError))
+    finally:
+        return r.content.decode()
 
 
 def ocr_space_url(url, overlay=False,  language='eng', ocrengine=1):
@@ -61,15 +86,10 @@ def ocr_space_url(url, overlay=False,  language='eng', ocrengine=1):
                'language': language,
                'OCREngine': ocrengine,
                }
-    r = requests.post('https://api.ocr.space/parse/image',
-                      data=payload,
-                      )
+    r = requests.post(url_api, data=payload)
     return r.content.decode()
 
 
-# Use examples:
-# test_file = ocr_space_file(filename='example_image.png', language='pol')
-# test_url = ocr_space_url(url='http://i.imgur.com/31d5L5y.jpg')
 if __name__ == "__main__":
-    # print(ocr_space_file(filename='example_image.png', language='pol'))
+    # print(ocr_space_file(filename='E:/temp/ru1.png', language='rus'))
     print(ocr_space_url(url='http://dl.a9t9.com/ocrbenchmark/eng.png'))
